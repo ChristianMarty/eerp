75de546dfdc5cd999b1609198797f872c0588ac5
ChristianMarty <info@christian-marty.ch>
2022-02-13 14:19:27 +0100

    backEnd: add currency.php

---
+++ BackEnd/apiFunctions/purchasing/currency.php
@@ -0,0 +1,35 @@
+<?php
+//*************************************************************************************************
+// FileName : currency.php
+// FilePath : apiFunctions/purchasing/
+// Author   : Christian Marty
+// Date		: 01.08.2020
+// License  : MIT
+// Website  : www.christian-marty.ch
+//*************************************************************************************************
+
+require_once __DIR__ . "/../databaseConnector.php";
+
+if($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+	$dbLink = dbConnect();
+	if($dbLink == null) return null;
+	
+	$query = "SHOW COLUMNS FROM purchasOrder LIKE 'Currency'";
+	
+	$output = array();
+	$option_array = array();
+	
+	$result = dbRunQuery($dbLink,$query);
+	if ($result) 
+	{
+		$result = mysqli_fetch_assoc($result)['Type'];
+		$option_array = explode("','",preg_replace("/(enum|set)\('(.+?)'\)/","\\2", $result));
+	}
+	
+	$output = $option_array;
+
+	dbClose($dbLink);	
+	sendResponse($output);
+}
+?>
