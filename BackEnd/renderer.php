036cd2a63a6f8d2d93e8dc103c6ded7d3b65c847
Christian Marty <info@christian-marty.ch>
2022-08-27 22:55:23 +0200

    backend: add renderer.php

---
+++ BackEnd/renderer.php
@@ -0,0 +1,68 @@
+<?php
+//*************************************************************************************************
+// FileName : renderer.php
+// FilePath : /
+// Author   : Christian Marty
+// Date		: 01.08.2021
+// Website  : www.christian-marty.ch
+//*************************************************************************************************
+
+require_once __DIR__ . "/config.php";
+global $devMode;
+global $showPhpError;
+
+if($showPhpError)
+{
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+}
+
+
+// TODO: Fix for prod version
+
+header("Access-Control-Allow-Methods: POST, PATCH, GET, OPTIONS"); 
+
+
+if($devMode)
+{
+	header("Access-Control-Allow-Origin: *");      
+	header("Access-Control-Allow-Headers: *");
+	//header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
+{
+	header("Allow: GET, PUT, OPTIONS"); 
+	exit;
+}
+
+session_start();
+
+$params = array();
+
+$apiRequestParts = explode('renderer.php/', $_SERVER['REQUEST_URI']);
+
+$filePath = "renderer/";
+$apiRequest = explode('?',$apiRequestParts[1])[0];
+$filePath .= $apiRequest;
+$filePath = rtrim($filePath, "/");
+$filePath .= ".php";
+
+if(!file_exists($filePath)) sendResponse(null, "Invalid URL");
+
+if( $apiRequest == "user/login" || $apiRequest == "user/logout" || $apiRequest == "user/info")
+{
+	require $filePath;
+}
+else if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)||$devMode) 
+{
+	require $filePath;
+}
+else
+{
+	sendResponse(null, "User Session Invalid. Please Log In.");
+}
+
+?>
+
