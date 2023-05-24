1911aeffd938667c951bff6b297996981b53ed74
Christian Marty <info@christian-marty.ch>
2023-05-24 18:40:53 +0200

    return ASH code on ASH item create

--- BackEnd/apiFunctions/assembly/unit/history/item.php
+++ BackEnd/apiFunctions/assembly/unit/history/item.php
@@ -133,7 +133,7 @@ else if($_SERVER['REQUEST_METHOD'] == 'POST')
 	$sqlData['EditToken']['raw'] = "history_generateEditToken()";
 	$query = dbBuildInsertQuery($dbLink,"assembly_unit_history", $sqlData);
 	
-	$query .= " SELECT EditToken FROM assembly_unit_history WHERE Id = LAST_INSERT_ID();";
+	$query .= " SELECT EditToken, AssemblyUnitHistoryNumber FROM assembly_unit_history WHERE Id = LAST_INSERT_ID();";
 	
 	$error = null;
 	$output = array();
@@ -143,6 +143,7 @@ else if($_SERVER['REQUEST_METHOD'] == 'POST')
 			if ($result = mysqli_store_result($dbLink)) {
 				while ($row = mysqli_fetch_row($result)) {
 					$output['EditToken'] = $row[0];
+                    $output['Barcode'] = "ASH-".$row[1];
 				}
 				mysqli_free_result($result);
 			}
