27f855ddef80ae8a5527985a8f9ee3d47e2a2cb6
Christian Marty <info@christian-marty.ch>
2024-04-08 19:28:55 +0200

    add new renderer

---
+++ BackEnd/renderer/boxLabel_treston-6020-4ESD.php
@@ -0,0 +1,172 @@
+<?php
+//*************************************************************************************************
+// FileName : Treston_6020-4ESD.php
+// FilePath : renderer/
+// Author   : Christian Marty
+// Date		: 02.12.2023
+// License  : MIT
+// Website  : www.christian-marty.ch
+//*************************************************************************************************
+declare(strict_types=1);
+global $database;
+global $api;
+
+require_once __DIR__ . "/../apiFunctions/util/_barcodeParser.php";
+require_once __DIR__ . "/../apiFunctions/util/_barcodeFormatter.php";
+require_once __DIR__ . "/../config.php";
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Inventory Label</title>
+<link  media="print" />
+</head>
+
+<style>
+    div.page{
+        position: absolute;
+        width:200mm;
+        height:271mm;
+
+        margin-top:11mm;
+        margin-left:5mm;
+    }
+    div.label{
+		table-layout: fixed;
+        float: left;
+        width:175mm;
+        height:41mm;
+        
+        border-style: dotted;
+        border-width: 1px;
+	}
+    div.margin{
+		table-layout: fixed;
+        float: left;
+        width:10mm;
+        height:41mm;
+        
+        border-style: solid;
+        border-width: 1px;
+	}
+	h1.title{
+		text-align: center;
+		font-size: 12mm;
+        margin-top: 4mm;
+        margin-bottom:2mm;
+	}
+    h2.title{
+		text-align: center;
+		font-size: 8mm;
+        margin-top: 4mm;
+        margin-bottom:2mm;
+	}
+    h1.box{
+		text-align: center;
+		font-size: 10mm;
+        margin-top: 5mm;
+        margin-bottom:2mm;
+	}
+    h2.box{
+		text-align: center;
+		font-size: 10mm;
+        margin-top: 4mm;
+        margin-bottom:2mm;
+	}
+    div.title {
+        table-layout: fixed;
+        float: left;
+
+        width:80%;
+        height:100%;
+    }
+    div.box {
+        table-layout: fixed;
+        float: left;
+
+        width:19%;
+        height:100%;
+        border-left: 5px solid;
+    }
+</style>
+
+<div class="page">
+
+<?php
+if($api->isGet())
+{
+    $parameter = $api->getGetData();
+
+    $locationNumbers = [];
+	if (isset($parameter->LocationNumber)) {
+        $locationNumbers = explode(",", $parameter->LocationNumber);
+
+        if(!count($locationNumbers)){
+            echo "Location number list empty";
+            exit;
+        }
+
+        foreach ($locationNumbers as &$item) {
+            $item = barcodeParser_LocationNumber($item);
+            unset($item);
+        }
+    }
+
+    $field_offset = 0;
+    if (isset($parameter->Offset)) {
+        $field_offset = $parameter->Offset;
+    }
+
+    $locationNumbersString = implode(", ", $locationNumbers);
+    if(strlen($locationNumbersString)=== 0){
+        echo "Location number list is empty.";
+        exit;
+    }
+    $query = <<< STR
+        SELECT 
+            LocationNumber,
+            Name,
+            Title,
+            Description
+        FROM location
+        WHERE LocationNumber IN( $locationNumbersString );
+    STR;
+
+    $rows = $database->query($query);
+
+	for ($i = 0; $i < $field_offset; $i++) {
+        echo "<div class='label'>";
+        echo "</div>";
+    }
+
+    global $rendererRootPath;
+
+	foreach ($rows as $row)
+    {
+        $title = $row->Title;
+        $description = $row->Description;
+        $name = $row->Name;
+        
+        $content  = "<div class='title'>";
+        $content .= "<h1 class='title'>$title</h1>";
+        $content .= "<h2 class='title'>$description</h2>";
+        $content .= "</div>";
+        
+        $content .= "<div class='box'>";
+        $content .= "<h1 class='box'>Box</h1>";
+        $content .= "<h2 class='box'>$name</h2>";
+        $content .= "</div>";
+
+        echo "<div class='margin'></div><div class='label'>";
+        echo $content;
+        echo "</div><div class='margin'></div>";
+    }
+    
+    
+}
+
+?>
+</div>
---
+++ BackEnd/renderer/locationBoxLabel_L4773_3pcs.php
@@ -0,0 +1,169 @@
+<?php
+//*************************************************************************************************
+// FileName : locationBoxLabel_L4773.php
+// FilePath : renderer/
+// Author   : Christian Marty
+// Date		: 02.12.2023
+// License  : MIT
+// Website  : www.christian-marty.ch
+//*************************************************************************************************
+declare(strict_types=1);
+global $database;
+global $api;
+
+require_once __DIR__ . "/../apiFunctions/util/_barcodeParser.php";
+require_once __DIR__ . "/../apiFunctions/util/_barcodeFormatter.php";
+require_once __DIR__ . "/../config.php";
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Inventory Label</title>
+<link  media="print" />
+</head>
+
+<style>
+    div.page{
+        position: absolute;
+        width:200mm;
+        height:271mm;
+
+        margin-top:11mm;
+        margin-left:2mm;
+
+    }
+    div.label{
+		table-layout: fixed;
+        float: left;
+        width:63.5mm;
+        height:33.9mm;
+		margin-left:1.25mm;
+		margin-right:1.25mm;
+		margin-top:0;
+		margin-bottom:0;
+
+	}
+	h1.label{
+		text-align: center;
+		font-size: 10mm;
+        margin-top: 0.5mm;
+        margin-bottom:0.5mm;
+	}
+	p.label{
+		text-align: center;
+		font-size: 4mm;
+		margin-top: 0.5mm;
+		margin-bottom:0.5mm;
+		font-weight: bold;
+	}
+    div.title {
+        table-layout: fixed;
+        float: left;
+        transform: rotate(90deg);
+
+        width:49%;
+        height:100%;
+
+        position: relative;
+        left: 30mm;
+    }
+    div.barcode {
+        table-layout: fixed;
+        float: left;
+        transform: rotate(90deg);
+
+        width:49%;
+        height:100%;
+
+        position: relative;
+        left: -42mm;
+    }
+	img.label{
+        width:30mm;
+        height:10mm;
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
+
+<div class="page">
+
+<?php
+if($api->isGet())
+{
+    $parameter = $api->getGetData();
+
+    $locationNumbers = [];
+	if (isset($parameter->LocationNumber)) {
+        $locationNumbers = explode(",", $parameter->LocationNumber);
+
+        if(!count($locationNumbers)){
+            echo "Location number list empty";
+            exit;
+        }
+
+        foreach ($locationNumbers as &$item) {
+            $item = barcodeParser_LocationNumber($item);
+            unset($item);
+        }
+    }
+
+    $field_offset = 0;
+    if (isset($parameter->Offset)) {
+        $field_offset = $parameter->Offset;
+    }
+
+    $locationNumbersString = implode(", ", $locationNumbers);
+    if(strlen($locationNumbersString)=== 0){
+        echo "Location number list is empty.";
+        exit;
+    }
+    $query = <<< STR
+        SELECT 
+            LocationNumber,
+            Name
+        FROM location
+        WHERE LocationNumber IN( $locationNumbersString );
+    STR;
+
+    $rows = $database->query($query);
+
+	for ($i = 0; $i < $field_offset; $i++) {
+        echo "<div class='label'>";
+        echo "</div>";
+    }
+
+    global $rendererRootPath;
+
+	foreach ($rows as $row)
+    {
+        foreach([1,2,3] as $i) // 3 labels each
+        {
+
+            $locNo = $row->LocationNumber . " ";
+            $name = $row->Name . " ";
+            $locationBarcode = barcodeFormatter_LocationNumber($locNo);
+
+            $content  = "<div class='title'>";
+            $content .= "<h1 class='label'>Box</h1>";
+            $content .= "<h1 class='label'>$name</h1>";
+            $content .= "</div>";
+
+            $content .= "<div class='barcode'>";
+            $content .= "<p class='label'>$locationBarcode</p>";
+            $content .= "<img class='label' src='" . $rendererRootPath . "/barcode/barcode?text=" . $locationBarcode . "'/>";
+            $content .= "</div>";
+
+            echo "<div class='label'>";
+            echo $content;
+            echo "</div>";
+        }
+    }
+}
+
+?>
+</div>
