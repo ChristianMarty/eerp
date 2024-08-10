8835dede1d1d23abd9b74dffe50cce892ac3c93a
Christian Marty <info@christian-marty.ch>
2024-08-10 23:24:23 +0200

    add stock kisok

---
+++ StockKioskJS/api.js
@@ -0,0 +1,83 @@
+import config from "./config.js";
+export async function getStockInformation(StockCode) {
+    if(StockCode==="" || StockCode===null){
+        return null;
+    }
+
+    var params = {
+        'user': config.userName,
+        'token': config.userToken,
+        'StockCode': StockCode
+    };
+
+    const url = config.apiUrl+"/stock/item?"+new URLSearchParams(params).toString();
+    let response  = await fetch(url);
+
+    if (!response.ok) {
+        return  null;
+    }
+    let data =  await response.json();
+    return  data.data;
+}
+
+export async function countStock(StockCode, Quantity, Note) {
+    if(StockCode==="" || StockCode===null){
+        return null;
+    }
+    if(Quantity==="" || Quantity===null){
+        return null;
+    }
+
+    const url = config.apiUrl+"/stock/history/item"
+    const postData = {
+        StockNumber: StockCode,
+        Quantity: Quantity,
+        Note: Note
+    }
+
+    let response  = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData)
+    })
+
+    if (!response.ok) {
+        return  null;
+    }
+    let data =  await response.json();
+    return  data.data;
+}
+
+export async function removeStock(StockCode, Quantity, Note) {
+    if(StockCode==="" || StockCode===null){
+        return null;
+    }
+    if(Quantity==="" || Quantity===null){
+        return null;
+    }
+
+    const url = config.apiUrl+"/stock/history/item"
+    const postData = {
+        StockNumber: StockCode,
+        RemoveQuantity: Quantity,
+        Note: Note
+    }
+
+    let response  = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData)
+    })
+
+    if (!response.ok) {
+        return  null;
+    }
+    let data =  await response.json();
+    return  data.data;
+}
+
+
---
+++ StockKioskJS/component.js
@@ -0,0 +1,52 @@
+
+function partInformation(stockData){
+    return `
+        <table>
+        <tr>
+          <th>Manufacturer:</th>
+          <td>${stockData.Part.ManufacturerName}</td>
+        
+        </tr>
+        <tr>
+          <th>Part Number:</th>
+          <td>${stockData.Part.ManufacturerPartNumber}</td>
+        </tr>
+        </table>
+`;
+}
+function locationInformation(stockData){
+    return `
+      <table>
+        <tr>
+          <th>Location:</th>
+          <td>${stockData.Location.Name}</td>
+        </tr>
+        <tr>
+          <th>Location Code:</th>
+          <td>${stockData.Location.Path}</td>
+        </tr>
+        <tr>
+          <th>Location Path:</th>
+          <td>${stockData.Location.ItemCode}</td>
+        </tr>
+      </table>
+`;
+}
+function stockInformation(stockData){
+    return `
+      <table>
+        <tr>
+          <th>Quantity:</th>
+          <td>${stockData.Quantity.Quantity}</td>
+        </tr>
+        <tr>
+          <th>Create Quantity:</th>
+          <td>${stockData.Quantity.CreateQuantity}</td>
+        </tr>
+        <tr>
+          <th>Days Since Stocktaking:</th>
+          <td>${stockData.Quantity.Certainty.DaysSinceStocktaking}</td>
+        </tr>
+      </table>
+`;
+}
