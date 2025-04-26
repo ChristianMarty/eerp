7fa0781d26ff221aa42bdb45c33d120d7e333491
Christian Marty <info@christian-marty.ch>
2025-04-26 17:09:14 +0200

    bugfix

--- FrontEnd/src/router/modules/metrology.js
+++ FrontEnd/src/router/modules/metrology.js
@@ -14,21 +14,21 @@ const metrologyRouter = {
       path: 'testSystem',
       component: () => import('@/views/metrology/testSystem/list.vue'),
       name: 'testSystemList',
-      meta: { title: 'Test System List', icon: 'el-icon-odometer' }
+      meta: { title: 'Test System List', icon: 'el-icon-odometer', roles: ['metrology.testSystem.view'] }
     },
     {
       path: 'testSystem/create',
       component: () => import('@/views/metrology/testSystem/create.vue'),
       name: 'testSystemCreate',
       meta: {
-        title: 'Create Test System', icon: 'el-icon-plus', roles: ['metrology.create']
+        title: 'Create Test System', icon: 'el-icon-plus', roles: ['metrology.testSystem.create']
       }
     },
     {
       path: 'testSystem/item/:TestSystemNumber(.*)',
       component: () => import('@/views/metrology/testSystem/item.vue'),
       name: 'testSystemView',
-      meta: { title: 'Item' },
+      meta: { title: 'Item', roles: ['metrology.testSystem.view'] },
       hidden: true
     }
 
